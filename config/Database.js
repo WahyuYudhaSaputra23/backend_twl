022a1a178d87da3f3c40ee297254b94c2fdb3529
WahyuYudhaSaputra <wyudhasaputra@gmail.com>
2023-07-14 22:56:56 +0700

    main

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,20 @@
 // Database.js: File ini berisi konfigurasi koneksi database menggunakan Sequelize. 
 // Melakukan koneksi ke database MySQL dengan mengatur nama database, 
 // username, password, host, dan jenis dialect.
-
+import dotenv from 'dotenv'; dotenv.config();
 import { Sequelize } from "sequelize";
-const db = new Sequelize('bjogpue4v8y3tcegaxwl', 'uvjcjcm0aedzaouo', 'ha1JOBZjmtNpY4rWqyGw', {
+// const db = new Sequelize('bjogpue4v8y3tcegaxwl', 'uvjcjcm0aedzaouo', 'ha1JOBZjmtNpY4rWqyGw', {
+//     host: 'bjogpue4v8y3tcegaxwl-mysql.services.clever-cloud.com',
+//     dialect: 'mysql',
+//     port: 3306,
+//     dialectModule: 'mysql2'
+//      // optional, disables logging SQL queries
+//   });
+  
+const db = new Sequelize(process.env.MYSQL_ADDON_URI, {
     host: 'bjogpue4v8y3tcegaxwl-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     port: 3306
-     // optional, disables logging SQL queries
   });
   
 
